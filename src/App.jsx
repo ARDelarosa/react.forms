f12695069f7bb7b96c625b4d4863443d0c0ca061
ARDelarosa <63564596+ARDelarosa@users.noreply.github.com>
2024-07-08 17:26:30 -0700

    add intial files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import Authenticate from "./components/Authenticate";
+import SignUpForm from "./components/SignUpForm";
+
+function App() {
+
+  return (
+    <>
+ 
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useState } from 'react';
+
+const [username, setUsername] = useState("");
+const [password, setPassword] = useState("");
+const [error, setError] = useState(null);
+
+function SignUpForm() {
+    return <>
+    <h2>Sign Up!</h2>; 
+    
+    <form>
+        <label>
+            Username: <input value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <label>
+            Password: <input value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <button>Submit</button>
+    </form>
+    </>
+}
+
+export default SignUpForm
